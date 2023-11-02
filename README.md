da1ac6507443feb3a14d662499d5a8f2e80f6db2
“kazuhikochikae” <kazuhikochikae@gmail.com>
2023-11-02 17:34:23 +0900

    step1微調整

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # markdown 記法でテーブルスキーマ（モデル名、カラム名、データ型）を記載する
 
-- モデル名：task.rb
-
-- カラム名：name 、データ型：string
-- カラム名：detail 、データ型：text
+| カラム名 | データ型 | モデル名 |
+| name | string | task |
+| detail | text |
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'tasks/index'
   resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root :to => 'tasks#index'
+  root 'tasks#index'
 end
