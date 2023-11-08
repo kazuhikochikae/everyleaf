75d9340dc6f974984a6a178bb589ea4906d3809e
“kazuhikochikae” <kazuhikochikae@gmail.com>
2023-11-08 14:11:10 +0900

    ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'tasks/index'
   resources :tasks
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'tasks#index'
 end
